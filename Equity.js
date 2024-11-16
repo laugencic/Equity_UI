0ee45fbfec8131d3037118fc19bcd4e6b4f31ecf
Lawrence <lawrencekamande23@gmail.com>
2024-11-16 14:26:35 +0300

    Equity

---
+++ Equity.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("dashboard").style.display='none';
+    document.querySelector("form").onsubmit=function(event){
+        event.preventDefault()
+        let username=document.getElementById("user").value;
+        let password=document.getElementById("pass").value;
+        let greeting;
+
+        function greet(){
+            let now=new Date()
+            let hour=now.getHours()
+            
+            if(hour <12){
+                greeting="Good morning"
+            }
+            else if(hour >=12 && hour <= 17){
+                greeting="Good afternoon";
+            }
+            else{
+                greeting="Good Evening"
+            }
+        }
+        greet()
+        if(username){
+            window.alert(`Welcome back ${username}`)
+        }
+        document.getElementById("greet").textContent=`${greeting} ${username}`
+
+        username=document.getElementById("user").value="";
+        document.getElementById("form").style.display='none'
+        document.getElementById("dashboard").style.display='block';
+
+    }
+})
